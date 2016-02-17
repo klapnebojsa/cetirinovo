057029398b8d854783a32474136afbd90acc587b
klapnebojsa <klapnebojsa@gmail.com>
2016-02-17 23:38:42 +0100

    .

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.async "0.2.374"]
                  [uncomplicate/clojurecl "0.3.0"]]
   :profiles {:dev {:dependencies [[midje "1.8.3"]]}})
